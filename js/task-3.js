b64a84d2b0735e232b1498ab170561a1db4e958c
Anush Biloshytska <biloshytska.anush@icloud.com>
2025-04-27 12:44:30 +0300

    hw8

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const textInput = document.querySelector('.js-text-input');
-const output = document.querySelector('.js-output'); 
-
-textInput.addEventListener('input', (event) => {
-    const handleInputEvent = event.currentTarget.value.trim();
-    output.textContent = handleInputEvent === '' ? 'Anonymous' : handleInputEvent;
-});
